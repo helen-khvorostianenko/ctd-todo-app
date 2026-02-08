3d4106c30fb92309d5572980fcfb2593dc0c92f4
Helen Allagulova <hele.allaguova@clazion.com>
2026-02-08 12:32:01 -0500

    duplicate state logic to reducer

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -1,8 +1,8 @@
 const initialState = {
   todoList: [],
   isLoading: false,
-  error: "",
-  isSaving: false
+  errorMessage: '',
+  isSaving: false,
 };
 
 const actions = {
@@ -29,42 +29,105 @@ export const reducer = (state = initialState, action) => {
     case actions.fetchTodos:
       return {
         ...state,
+        isLoading: true,
       };
-    case actions.loadTodos:
+    case actions.loadTodos: {
+      const fetchedRows = action.records.map((record) => {
+        const row = {
+          id: record.id,
+          ...record.fields,
+        };
+        row.isCompleted = row.isCompleted ?? false;
+        return row;
+      });
       return {
         ...state,
+        todoList: fetchedRows,
+        isLoading: false,
       };
-    case actions.setLoadError:
+    }
+    case actions.setLoadError: 
       return {
         ...state,
+        errorMessage: action.error.message,
+        isLoading: false,
       };
     case actions.startRequest:
       return {
         ...state,
+        isSaving: true,
       };
-    case actions.addTodo:
+    case actions.addTodo: {
+      const records = action.records;
+      if (!records?.[0]) {
+        return {
+          ...state,
+          errorMessage: 'No records returned from Airtable API',
+          isSaving: false,
+        };
+      }
+
+      const savedTodo = {
+        id: records[0].id,
+        ...records[0].fields,
+      };
+      if (!records[0].fields.isCompleted) {
+        savedTodo.isCompleted = false;
+      }
+
       return {
         ...state,
+        todoList: [...state.todoList, savedTodo],
+        isSaving: false,
       };
+    }
     case actions.endRequest:
       return {
         ...state,
+        isLoading: false,
+        isSaving: false,
       };
-    case actions.updateTodo:
-      return {
-        ...state,
-      };
-    case actions.completeTodo:
-      return {
-        ...state,
-      };
+
     case actions.revertTodo:
+    case actions.updateTodo: {
+      const editedTodo = action.editedTodo;
+
+      const originalTodo = state.todoList.find(
+        (todo) => todo.id === editedTodo.id
+      );
+      if (!originalTodo) return state;
+
+      const updatedTodos = state.todoList.map((item) =>
+        item.id === editedTodo.id ? { ...editedTodo } : item
+      );
+      const updatedState = {
+        ...state,
+        todoList: updatedTodos,
+      };
+
+      if (action.error) {
+        updatedState.errorMessage = action.error.message;
+      }
+
+      return updatedState;
+    }
+    case actions.completeTodo: {
+      const updatedTodos = state.todoList.map((item) =>
+        item.id === action.id ? { ...item, isCompleted: true } : item
+      );
+
+      const completedTodo = updatedTodos.find((item) => item.id === action.id);
+      if (!completedTodo) return state;
+
       return {
         ...state,
+        todoList: updatedTodos,
       };
+    }
     case actions.clearError:
       return {
         ...state,
+        errorMessage: '',
       };
     default:
       return state;
@@ -72,4 +135,4 @@ export const reducer = (state = initialState, action) => {
 
 }
 
-export { initialState, actions };
+export { initialState, actions, reducer };
